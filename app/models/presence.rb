18f106595ddd0d4aea75aecab3319eadf8ff6a14
charles noppe <noppe.charles@gmail.com>
2019-07-11 16:58:13 +0200

    form presences

--- app/models/presence.rb
+++ app/models/presence.rb
@@ -1,3 +1,7 @@
 class Presence < ApplicationRecord
   belongs_to :user
+  validates :nuit_jeudi, presence: true
+  validates :nuit_samedi, presence: true
+  validates :nuit_vendredi, presence: true
+  validates :mariage, presence: true
 end
