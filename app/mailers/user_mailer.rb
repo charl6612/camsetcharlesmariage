dd41583ce986927168e9e7f8d1c590a3b934e9f7
charles noppe <noppe.charles@gmail.com>
2019-10-16 16:12:29 +0200

    fix credential

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,7 +1,11 @@
 class UserMailer < ApplicationMailer
-  def welcome
-    @user = params[:user] # Instance variable => available in view
-    mail(to: @user.email, subject: 'Welcome to Le Wagon')
-    # This will render a view in `app/views/user_mailer`!
+  def send_simple_message
+    RestClient.post "https://api:b47be5e4849315fe3ccccfa635444286-c27bf672-98da50cf"\
+    "@api.mailgun.net/v3/YOUR_DOMAIN_NAME/messages",
+    :from => "Excited User <mailgun@YOUR_DOMAIN_NAME>"
+    :to => "bar@example.com, YOU@YOUR_DOMAIN_NAME",
+    :subject => "Hello",
+    :text => "Testing some Mailgun awesomness!"
   end
+  
 end
